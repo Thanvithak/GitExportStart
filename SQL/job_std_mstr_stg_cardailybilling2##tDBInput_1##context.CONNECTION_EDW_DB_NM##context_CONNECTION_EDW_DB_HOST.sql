85b964af9b75d7df3206e6754f0b799ebccb508e
Thanvithak <53471226+Thanvithak@users.noreply.github.com>
2022-05-31 14:31:29 +0530

    Add files via upload

---
+++ SQL/job_std_mstr_stg_cardailybilling2##tDBInput_1##context.CONNECTION_EDW_DB_NM##context_CONNECTION_EDW_DB_HOST.sql
@@ -0,0 +1,60 @@
+WITH  gl_acc_keys as (select GL_ACCOUNT_KEY as GL_ACCT_KEY from DIM_GL_ACCOUNT_T
+where GL_ACCOUNT_NUM in ('5400035', '5400045', '5410000', '5431011','6047760', '5400256', '5400201', '5400275', '5400285', '6046040', '5400222', '5400267', '5400110','5400203','1180104',
+'1180108', '1180109', '1180113', '5400002', '4313000')),
+
+bill_cycle_tbl as (  
+	select distinct dbct.bill_cycle_code_num bill_cycle_code_num, 
+  first_value(bill_cycle_key) over (partition by dbct.bill_cycle_code_num order by dbct.BILL_CYCLE_END_DT desc) as bill_cycle_key
+	from 
+		DIM_BILL_CYCLE_T dbct
+	where
+		extract (year from dbct.ROW_EXP_DT) = 9999 and  
+    dbct.BILL_CYCLE_END_DT < TO_DATE( context.JOB_BUSINESS_PROCESSING_DATE ,'YYYY-MM-DD')
+),
+
+cust_bill_cycle_tbl as (
+	select sdt.customer_id customer_id, dct.customer_key customer_key, 
+  sdt.master_subscriber_id master_subscriber_id, dst.subscriber_key subscriber_key, bct.bill_cycle_key  
+	from 
+		context.CONNECTION_EDW_DB_USER_WRK  .STG_PRE_CAR_DLY_DELTA sdt
+	join
+		dim_customer_t dct
+	on
+		(dct.customer_id = sdt.customer_id)
+	join
+		dim_subscriber_t dst
+	on
+		dst.master_subscriber_id = sdt.master_subscriber_id
+	join
+		bill_cycle_tbl bct
+	on	
+		(bct.BILL_CYCLE_CODE_NUM = 
+		case 
+		when VALIDATE_CONVERSION(sdt.BILL_CYCLE_CD AS NUMBER) = 0 then null
+		else to_number(sdt.BILL_CYCLE_CD)
+		end) 
+),
+
+bill_last_sscr_amt as (  
+select cbct.master_subscriber_id master_subscriber_id, 
+SUM(fsrgst.RCRR_AMT) as BILL_LAST_SSCR_MRC_AMT,
+SUM(fsrgst.ONE_TM_DISC_AMT+fsrgst.RCRR_DISC_AMT+fsrgst.USG_DISC_AMT) as BILL_LAST_SSCR_DISC_AMT
+from 
+	cust_bill_cycle_tbl cbct
+join 
+	FTA_SSCR_REV_GL_SUMM_T fsrgst
+on 
+	(cbct.customer_key = fsrgst.cust_key and cbct.subscriber_key = fsrgst.sscr_key and cbct.bill_cycle_key = fsrgst.bill_cycl_key)
+join
+	gl_acc_keys gl
+on
+	gl.GL_ACCT_KEY = fsrgst.GL_ACCT_KEY
+group by 
+	cbct.master_subscriber_id)
+  
+SELECT /*+ context.MAIN_SQL_HINT */
+TEMP1.master_subscriber_id AS SSCR_MSTR_ID
+,bill_last_sscr_amt.BILL_LAST_SSCR_MRC_AMT
+,bill_last_sscr_amt.BILL_LAST_SSCR_DISC_AMT
+FROM context.CONNECTION_EDW_DB_USER_WRK  .STG_PRE_CAR_DLY_DELTA  TEMP1
+LEFT JOIN bill_last_sscr_amt ON (bill_last_sscr_amt.master_subscriber_id = TEMP1.master_subscriber_id)
---
+++ SQL/job_std_mstr_stg_cardailybods##tDBInput_1##context.CONNECTION_ODS_DB_NM##context_CONNECTION_ODS_DB_HOST.sql
@@ -0,0 +1,19 @@
+With delta_sscr as 
+(select distinct subscriber_id from context.CONNECTION_EDW_DB_USER_WRK.STG_PRE_CAR_DLY_DELTA@context.CONNECTION_EDW_DBLINK_NAME)
+--SELECT /*+ parallel(rxn,16) full(rxn) parallel(role,16) full(role) parallel(part,16) full(part) parallel(d,16) full(d)*/
+SELECT /*+ context.MAIN_SQL_HINT */
+ part.instance_id AS subscriber_id,
+ rxn.start_time
+FROM  table_intrxn rxn, table_con_sp_role role, table_site_part part, delta_sscr d
+WHERE active = 1
+ and d.subscriber_id = part.instance_id
+ AND rxn.intrxn2contact = role.con_sp_role2contact
+ AND role.con_sp_role2site_part = part.objid
+ AND CASE
+		 WHEN rxn.notes LIKE '%UPGRADE%' THEN 1
+		 WHEN rxn.notes LIKE '%upgrade%' THEN 1
+		 WHEN rxn.notes LIKE '%Upgrade%' THEN 1
+		 ELSE 0
+	 END > 0
+GROUP BY
+ part.instance_id, rxn.start_time
